cab5659c2ce1b0f6a3d73bf5835ba32ea955c3c3
Herbert, David J <darb1@bas.ac.uk>
2015-11-16 13:18:16 +0000

    Creation developments

--- target/mapengine-1.0.0/static/js/modules/Common.js
+++ target/mapengine-1.0.0/static/js/modules/Common.js
@@ -478,6 +478,17 @@ magic.modules.Common = function () {
             return(converted);
         },
         /**
+         * Create a UUID
+         * http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript answer by broofa
+         * @returns {string}
+         */
+        uuid: function() {
+            'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = crypto.getRandomValues(new Uint8Array(1))[0]%16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        },
+        /**
          * Degrees to radians
          * @param {float} degs
          * @returns {float}
