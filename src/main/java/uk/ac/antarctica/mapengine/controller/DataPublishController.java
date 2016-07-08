8e9d4665160316e924957c6f1eff1d8cdfc2fb77
Herbert, David J <darb1@bas.ac.uk>
2016-07-08 18:31:46 +0100

    More data publisher

--- src/main/java/uk/ac/antarctica/mapengine/controller/DataPublishController.java
+++ src/main/java/uk/ac/antarctica/mapengine/controller/DataPublishController.java
@@ -3,12 +3,16 @@
  */
 package uk.ac.antarctica.mapengine.controller;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,10 +27,35 @@ public class DataPublishController {
 
     @RequestMapping(value = "/publish_postgis", method = RequestMethod.POST, consumes = "multipart/form-data", produces = {"application/json"})
     public ResponseEntity<String> publishToPostGIS(MultipartHttpServletRequest request) throws Exception {
-        int count = 1;        
+        ResponseEntity<String> ret = null;
+        int count = 1;
         String sep = System.getProperty("file.separator");
-        String uploadDirName = System.getProperty("java.io.tmpdir") + sep + "upload" + Calendar.getInstance().getTimeInMillis();
-        for (MultipartFile mpf : request.getFileMap().values()) {            
+        String wdBase = System.getProperty("java.io.tmpdir") + sep + "upload_";
+        ArrayList<String> statusMessages = new ArrayList();
+        for (MultipartFile mpf : request.getFileMap().values()) {
+            String stdName = standardiseName(mpf.getOriginalFilename());
+            String extension = FilenameUtils.getExtension(stdName);
+            File wd = new File(wdBase + Calendar.getInstance().getTimeInMillis());
+            try {
+                if (wd.mkdir()) {
+                    /* Created the working directory */
+                    if (extension.equals("gpx")) {
+                        statusMessages.add(publishGpx(mpf, wd));
+                    } else if (extension.equals("kml")) {
+                        statusMessages.add(publishKml(mpf, wd));
+                    } else if (extension.equals("csv")) {
+                        statusMessages.add(publishCsv(mpf, wd));
+                    } else if (extension.equals("zip")) {
+                        statusMessages.add(publishShp(mpf, wd));
+                    }
+                } else {
+                    /* Failed to create */
+                    ret = PackagingUtils.packageResults(HttpStatus.INTERNAL_SERVER_ERROR, null, "Failed to create temporary working dir " + wd.getName());
+                }
+            } catch(Exception ex) {
+                ret = PackagingUtils.packageResults(HttpStatus.INTERNAL_SERVER_ERROR, null, "Error creating temporary working dir " + wd.getName() + " - " + ex.getMessage());
+            }
+            
             System.out.println("*** File no " + count);
             System.out.println("Original name : " + mpf.getOriginalFilename());
             System.out.println("Name : " + mpf.getName());
@@ -34,8 +63,32 @@ public class DataPublishController {
             System.out.println("Size : " + mpf.getSize());
             System.out.println("*** End of file no " + count);
             count++;
-        }
-        return(PackagingUtils.packageResults(HttpStatus.OK, null, "ok"));
+        }        
+        return(ret);
+    }
+    
+    @Transactional
+    private String publishGpx(MultipartFile mpf, File wd) {
+        String message = "";
+        return(message);
+    }
+    
+    @Transactional
+    private String publishKml(MultipartFile mpf, File wd) {
+        String message = "";
+        return(message);
+    }
+    
+    @Transactional
+    private String publishCsv(MultipartFile mpf, File wd) {
+        String message = "";
+        return(message);
+    }
+    
+    @Transactional
+    private String publishShp(MultipartFile mpf, File wd) {
+        String message = "";
+        return(message);
     }
     
     /** 
@@ -46,7 +99,7 @@ public class DataPublishController {
     private String standardiseName(String fileName) {
         String stdName = "";
         if (fileName != null && !fileName.isEmpty()) {
-            stdName = fileName.toLowerCase().replaceAll("[^a-z0-9]", "_").replaceAll("_{2,}", "_");
+            stdName = fileName.toLowerCase().replaceAll("[^a-z0-9.]", "_").replaceAll("_{2,}", "_");
         }
         return(stdName);
     }
