4dc5eedf78def68385b646ff4cb50489ce15f426
NERC\darb1 <darb1@bas.ac.uk>
2017-04-06 14:45:44 +0100

    Information tool

--- src/main/webapp/static/js/components/InfoModal.js
+++ src/main/webapp/static/js/components/InfoModal.js
@@ -27,6 +27,9 @@ magic.classes.InfoModal = function(options) {
  */
 magic.classes.InfoModal.prototype.getBackgroundInfo = function() {
     if (this.mapinfo == null && this.infolink != null) {
+        if (this.infolink.indexOf(magic.config.paths.baseurl) != 0) {
+            this.infolink = magic.config.paths.baseurl + "/proxy?url=" + encodeURIComponent(this.infolink);
+        }
         jQuery("#information-background").load(this.infolink, function() {
             jQuery('a[href="#information-background"]').tab("show");
         });
