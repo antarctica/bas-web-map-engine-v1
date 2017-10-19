316bb2f0c77a9dead12808d9735bcd4a03144e99
NERC\darb1 <darb1@bas.ac.uk>
2017-10-19 18:40:45 +0100

    Allowing multiple user roles/authorities for CCAMLR

--- src/main/java/uk/ac/antarctica/mapengine/controller/OgcServicesController.java
+++ src/main/java/uk/ac/antarctica/mapengine/controller/OgcServicesController.java
@@ -509,9 +509,11 @@ public class OgcServicesController implements ServletContextAware {
         }
         
         if (secured) {
-            /* Obtain credentials for each user role and test whether the requested data is available with each set in turn */      
+            /* Obtain credentials for each user role and test whether the requested data is available with each set in turn */
+            System.out.println("Test for secured layer");
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             if (auth != null) {
+                System.out.println("Authenticated user found");
                 for (GrantedAuthority ga : auth.getAuthorities()) {
                     String[] creds = ga.getAuthority().split(":");                                               
                     if (creds[0].equals("geoserver") && creds.length == 4) {
@@ -519,14 +521,17 @@ public class OgcServicesController implements ServletContextAware {
                         credsProvider.setCredentials(
                             new AuthScope(AuthScope.ANY),
                             new UsernamePasswordCredentials(creds[2], creds[3]));
+                        System.out.println("Test data access with credentials: username = " + creds[2] + ", password = " + creds[3]);
                         builder.setDefaultCredentialsProvider(credsProvider);
                         httpclient = builder.build();                            
                         CloseableHttpResponse httpResponse = httpclient.execute(httpget);
                         status = httpResponse.getStatusLine().getStatusCode();    
                         if (status < 400) {
+                            System.out.println("Successful access - status = " + status);
                             authorisedContent = httpResponse.getEntity().getContent();
                             break;
                         } else {
+                            System.out.println("Failed access - status = " + status + ", continue trying other authorities for user");
                             httpclient.close();
                             httpclient = null;
                         }                        
@@ -536,23 +541,27 @@ public class OgcServicesController implements ServletContextAware {
         }
         
         if (authorisedContent == null && status != 401) {
-            /* No authentication present, or we have tried and failed with all available user authroities */
+            /* No authentication present, or we have tried and failed with all available user authorities */
+            System.out.println("Anonymous access attempt");
             builder.setDefaultCredentialsProvider(null);
             httpclient = builder.build();                            
             CloseableHttpResponse httpResponse = httpclient.execute(httpget);
             status = httpResponse.getStatusLine().getStatusCode();    
             if (status < 400) {
+                System.out.println("Anonymous access successful");
                 authorisedContent = httpResponse.getEntity().getContent();
             }
         }
         
         if (authorisedContent != null) {
+            System.out.println("Sending content...");
             response.setContentType(mimeType);
             IOUtils.copy(authorisedContent, response.getOutputStream()); 
             if (httpclient != null) {
                 httpclient.close();
             }
         } else {
+            System.out.println("Content unauthorised - raising RestrictedDataException...");
             if (httpclient != null) {
                 httpclient.close();
             }
