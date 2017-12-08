a6c295aee5c7248dbd24e94679ef8440c4869675
NERC\darb1 <darb1@bas.ac.uk>
2017-12-08 16:21:15 +0000

    Feedback form now subclass of navigational tool form

--- src/main/webapp/static/js/components/navtools/Feedback.js
+++ src/main/webapp/static/js/components/navtools/Feedback.js
@@ -2,159 +2,181 @@
 
 magic.classes.Feedback = function(options) {
         
-    /* API options */
+    options = jQuery.extend({}, {
+        id: "feedback-tool",
+        layername: null,
+        caption: "Feedback on service or data issues",
+        popoverClass: "feedback-tool-popover",
+        popoverContentClass: "feedback-tool-popover-content"
+    }, options);
+
+    magic.classes.NavigationBarTool.call(this, options);
     
-    /* Identifier */
-    this.id = options.id || "feedback";
+    /* Callbacks */
+    this.setCallbacks({
+        onActivate: jQuery.proxy(this.onActivateHandler, this),
+        onDeactivate: jQuery.proxy(function() {
+            this.target.popover("hide");
+        }, this), 
+        onMinimise: jQuery.proxy(function() {
+            this.saveState();
+        }, this)
+    });
     
-    /* Button invoking the feedback form */
-    this.target = jQuery("#" + options.target); 
+    /* Form input names */
+    this.inputs = ["trackerId", "subject", "description", "reporter"];
     
-    /* Map */
-    this.map = options.map || magic.runtime.map;
+    /* Saved state */
+    this.savedState = {};
     
-    this.template = 
-        '<div class="popover popover-auto-width feedback-popover" role="popover">' + 
-            '<div class="arrow"></div>' +
-            '<h3 class="popover-title"></h3>' + 
-            '<div class="popover-content" style="width: 350px"></div>' + 
-        '</div>';
-    this.content = 
+    this.target.popover({
+        template: this.template,
+        title: this.titleMarkup(),
+        container: "body",
+        html: true,
+        content: this.markup()
+    })
+    .on("shown.bs.popover", jQuery.proxy(function() {        
+        this.activate();
+        if (this.savedState && !jQuery.isEmptyObject(this.savedState)) {
+            this.restoreState();
+        }   
+    }, this));
+};
+    
+magic.classes.Feedback.prototype = Object.create(magic.classes.NavigationBarTool.prototype);
+magic.classes.Feedback.prototype.constructor = magic.classes.Feedback;
+    
+magic.classes.Feedback.prototype.markup = function() {
+    return(
         '<div id="' + this.id + '-content">' +                                   
-            '<form id="' + this.id + '-feedback-form" class="form-horizontal" role="form">' +
+            '<form id="' + this.id + '-feedback-form" class="form" role="form">' +
                 '<input type="hidden" id="' + this.id + '-payload"></input>' + 
                 '<div class="panel">' +
-                    '<div class="panel-body alert-info feedback-info-text">' + 
+                    '<div class="panel-body alert-info">' + 
                     'Your chance to improve service quality by reporting data or interface problems. Give a short description below e.g. ' +
                     '&quot;Contours appear in the sea&quot; or &quot;Failed to find a certain place-name&quot; and configure the map to show the issue.  ' + 
                     'The complete state of the map will be saved automatically' + 
                     '</div>' + 
                 '</div>' +
-                '<div class="form-group form-group-sm col-sm-12">' +
-                    '<label class="col-sm-4" for="' + this.id + '-trackerId">Issue</label>' + 
-                    '<div class="col-sm-8">' + 
-                        '<select name="trackerId" id="' + this.id + '-trackerId" class="form-control" ' +
-                            'data-toggle="tooltip" data-placement="right" ' + 
-                            'title="What type of problem is this?">' + 
-                            '<option value="4">Data</option>' + 
-                            '<option value="1">Interface</option>' + 
-                        '</select>' +                            
-                    '</div>' + 
+                '<div class="form-group">' +
+                    '<label for="' + this.id + '-trackerId">This is an issue with</label>' + 
+                    '<select name="trackerId" id="' + this.id + '-trackerId" class="form-control" ' +
+                        'data-toggle="tooltip" data-placement="right" ' + 
+                        'title="What type of problem is this?">' + 
+                        '<option value="4">Data</option>' + 
+                        '<option value="1">Interface</option>' + 
+                    '</select>' +                            
                 '</div>' +
-                '<div class="form-group form-group-sm col-sm-12">' +
-                    '<label class="col-sm-4" for="' + this.id + '-subject">One-liner</label>' + 
-                    '<div class="col-sm-8">' + 
-                        '<input type="text" name="subject" id="' + this.id + '-subject" class="form-control" ' + 
-                            'placeholder="One line problem description" maxlength="150" ' + 
-                            'data-toggle="tooltip" data-placement="right" ' + 
-                            'title="Short problem description (required)" ' + 
-                            'required="required">' +
-                        '</input>' +    
-                    '</div>' + 
+                '<div class="form-group">' +
+                    '<label for="' + this.id + '-subject">One line summary</label>' + 
+                    '<input type="text" name="subject" id="' + this.id + '-subject" class="form-control" ' + 
+                        'placeholder="Short outline of the problem" maxlength="150" ' + 
+                        'data-toggle="tooltip" data-placement="right" ' + 
+                        'title="Short problem description (required)" ' + 
+                        'required="required">' +
+                    '</input>' +    
                 '</div>' +  
-                '<div class="form-group form-group-sm col-sm-12">' +
-                    '<label class="col-sm-4" for="' + this.id + '-description">Details</label>' +
-                    '<div class="col-sm-8">' + 
-                        '<textarea name="description" id="' + this.id + '-description" class="form-control" ' + 
-                            'style="height:8em !important" ' + 
-                            'placeholder="More detailed problem description" ' + 
-                            'data-toggle="tooltip" data-placement="right" ' + 
-                            'title="Longer description of the problem (required)" ' + 
-                            'required="required">' +                                           
-                        '</textarea>' + 
-                    '</div>' + 
+                '<div class="form-group">' +
+                    '<label for="' + this.id + '-description">Detailed description</label>' +
+                    '<textarea name="description" id="' + this.id + '-description" class="form-control" ' + 
+                        'style="height:5em !important" ' + 
+                        'placeholder="More details about the problem" ' + 
+                        'data-toggle="tooltip" data-placement="right" ' + 
+                        'title="Longer description of the problem (required)" ' + 
+                        'required="required">' +                                           
+                    '</textarea>' + 
                 '</div>' +
-                '<div class="form-group form-group-sm col-sm-12">' +
-                    '<label class="col-sm-4" for="' + this.id + '-reporter">Email</label>' + 
-                    '<div class="col-sm-8">' + 
-                        '<input type="email" name="reporter" id="' + this.id + '-reporter" class="form-control" ' + 
-                            'placeholder="Your email address" maxlength="150" ' + 
-                            'data-toggle="tooltip" data-placement="right" ' + 
-                            'title="Your email (required - used to communicate with you about this issue and for no other purpose)" ' + 
-                            'required="required">' +
-                        '</input>' +    
-                    '</div>' + 
+                '<div class="form-group">' +
+                    '<label for="' + this.id + '-reporter">Your email address</label>' + 
+                    '<input type="email" name="reporter" id="' + this.id + '-reporter" class="form-control" ' + 
+                        'placeholder="Not used for any other purpose" maxlength="150" ' + 
+                        'data-toggle="tooltip" data-placement="right" ' + 
+                        'title="Your email (required - used to communicate with you about this issue and for no other purpose)" ' + 
+                        'required="required">' +
+                    '</input>' +    
                 '</div>' +                            
-                '<div class="form-group form-group-sm col-sm-12">' +
+                '<div>' +
                     '<button id="' + this.id + '-go" class="btn btn-primary" type="button" ' + 
                         'data-toggle="tooltip" data-placement="right" title="Send feedback">' + 
                         '<span class="glyphicon glyphicon-send"></span>' + 
                     '</button>' +                        
                 '</div>' +                     
             '</form>' +               
-        '</div>';
-    this.target.popover({
-        template: this.template,
-        title: '<span><big><strong>Feedback</strong></big><button type="button" class="close">&times;</button></span>',
-        container: "body",
-        html: true,            
-        content: this.content
-    }).on("shown.bs.popover", jQuery.proxy(function() {
-        /* Set button handlers */
-        jQuery("#" + this.id + "-go").click(jQuery.proxy(function(evt) {
-            jQuery(evt.currentTarget).tooltip("hide");  /* Get rid of annoying persistent tooltip - not sure why... */
-            var formdata = {
-                payload: this.mapPayload()
-            };
-            var ok = true;
-            jQuery("#" + this.id + "-payload").val(formdata.payload);
-            jQuery("#" + this.id + "-feedback-form")[0].checkValidity();
-            jQuery.each(["trackerId", "subject", "description", "reporter"], jQuery.proxy(function(idx, elt) {
-                var fip = jQuery("#" + this.id + "-" + elt);
-                var fg = fip.closest("div.form-group");
-                var fstate = fip.prop("validity");
-                if (fstate.valid) {
-                    if (elt == "description") {
-                        formdata[elt] = JSON.stringify(jQuery.extend({}, this.mapPayload(), {"description": fip.val()}));
-                    } else {
-                        formdata[elt] = fip.val();
-                    }
-                    fg.removeClass("has-error").addClass("has-success");
-                } else {
-                    ok = false;
-                    fg.removeClass("has-success").addClass("has-error");
+        '</div>'
+    );
+};
+
+magic.classes.Feedback.prototype.onActivateHandler = function() {
+   
+    /* Set send button handler */
+    jQuery("#" + this.id + "-go").click(jQuery.proxy(function(evt) {
+        jQuery(evt.currentTarget).tooltip("hide");  /* Get rid of annoying persistent tooltip - not sure why... */        
+        if (this.validate()) {  
+            var formdata = this.formToPayload();
+            formdata.description = JSON.stringify(jQuery.extend({}, this.mapPayload(), {"description": formdata.description}));
+            var jqXhr = jQuery.ajax({
+                url: magic.config.paths.baseurl + "/feedback",
+                method: "POST",
+                processData: false,
+                data: JSON.stringify(formdata),
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": jQuery("meta[name='_csrf']").attr("content")
                 }
+            });
+            jqXhr.done(jQuery.proxy(function(response) {                        
+                bootbox.alert(
+                    '<div class="alert alert-info" style="margin-bottom:0">' + 
+                        '<p>Successfully sent your feedback</p>' + 
+                    '</div>'
+                );
+                this.deactivate();
             }, this));
-            if (ok) {                
-                var csrfHeaderVal = jQuery("meta[name='_csrf']").attr("content"); 
-                this.target.popover("hide");
-                var jqXhr = jQuery.ajax({
-                        url: magic.config.paths.baseurl + "/feedback",
-                        method: "POST",
-                        processData: false,
-                        data: JSON.stringify(formdata),
-                        headers: {
-                            "Content-Type": "application/json",
-                            "X-CSRF-TOKEN": csrfHeaderVal
-                        }
-                    });
-                    jqXhr.done(jQuery.proxy(function(response) {                        
-                        bootbox.alert(
-                            '<div class="alert alert-info" style="margin-bottom:0">' + 
-                                '<p>Successfully sent your feedback</p>' + 
-                            '</div>'
-                        );
-                    }, this));
-                    jqXhr.fail(function(xhr, status, err) {
-                        var detail = JSON.parse(xhr.responseText)["detail"];
-                        bootbox.alert(
-                            '<div class="alert alert-warning" style="margin-bottom:0">' + 
-                                '<p>Error occurred - details below:</p>' + 
-                                '<p>' + detail + '</p>' + 
-                            '</div>'
-                        );
-                    });                
-            }
-        }, this));
-        /* Close button */
-        jQuery(".feedback-popover").find("button.close").click(jQuery.proxy(function() { 
-            this.target.popover("hide");
-        }, this));
-    }, this));           
+            jqXhr.fail(function(xhr) {
+                var detail = JSON.parse(xhr.responseText)["detail"];
+                bootbox.alert(
+                    '<div class="alert alert-warning" style="margin-bottom:0">' + 
+                        '<p>Error occurred - details below:</p>' + 
+                        '<p>' + detail + '</p>' + 
+                    '</div>'
+                );
+            });                
+        } else {
+            bootbox.alert(
+                '<div class="alert alert-danger" style="margin-bottom:0">' + 
+                    '<p>Please correct the errors marked in your input</p>' + 
+                '</div>'
+            );
+        }
+    }, this));       
 };
 
 magic.classes.Feedback.prototype.interactsMap = function () {
     return(false);
+};
+
+magic.classes.Feedback.prototype.saveState = function() {    
+    this.savedState = this.formToPayload();
+};
+
+magic.classes.Feedback.prototype.restoreState = function() {
+    this.payloadToForm(this.savedState);
+    this.savedState = {};
+};
+
+magic.classes.Feedback.prototype.payloadToForm = function(payload) {
+    jQuery.each(this.inputs, jQuery.proxy(function(idx, ip) {
+        jQuery("#" + this.id + "-" + ip).val(payload[ip]);
+    }, this));
+};
+
+magic.classes.Feedback.prototype.formToPayload = function() {
+    var payload = {};
+    jQuery.each(this.inputs, jQuery.proxy(function(idx, ip) {
+        payload[ip] = jQuery("#" + this.id + "-" + ip).val();
+    }, this));
+    return(payload);
 };
 
 /**
@@ -179,4 +201,21 @@ magic.classes.Feedback.prototype.mapPayload = function() {
         payload.userAgent = navigator.userAgent;
     }
     return(payload);
+};
+
+magic.classes.Feedback.prototype.validate = function() {
+    var ok = true;
+    jQuery("#" + this.id + "-feedback-form")[0].checkValidity();
+    jQuery.each(this.inputs, jQuery.proxy(function(idx, ip) {
+        var fip = jQuery("#" + this.id + "-" + ip);
+        var fg = fip.closest("div.form-group");
+        var fstate = fip.prop("validity");
+        if (fstate.valid) {            
+            fg.removeClass("has-error").addClass("has-success");
+        } else {
+            ok = false;
+            fg.removeClass("has-success").addClass("has-error");
+        }
+    }, this));
+    return(ok);
 };
--- src/main/webapp/static/js/components/plugins/IssueInformation.js
+++ src/main/webapp/static/js/components/plugins/IssueInformation.js
@@ -5,41 +5,21 @@ magic.classes.IssueInformation = function(options) {
     /* API options */
     
     /* id of menu link that activates profile change form */
-    this.target = options ? options.target : "issue-info";
+    this.target = jQuery(".issue-info");
     
     /* Issue data (in Redmine JSON format) - probably very BAS-specific */
-    this.issueData = options ? options.data : magic.runtime.map_context.issuedata;
+    this.issueData = magic.runtime.map_context.issuedata;
   
-    /* Set up link handler */    
-    jQuery("#" + this.target).click(jQuery.proxy(function(evt) {
-        evt.stopPropagation();
-        var contentDiv = jQuery(evt.currentTarget).next("div");                
-        if (contentDiv) {
-            contentDiv.toggleClass("hidden");
-            if (!jQuery.isEmptyObject(this.issueData)) {                
-                contentDiv.html(
-                    '<div class="panel panel-default">' + 
-                        '<div class="panel-body">' + 
-                            '<table class="table table-condensed table-striped table-bordered">' + 
-                                '<tr><th>ID</th><td>' + this.issueData.id + '</td></tr>' + 
-                                '<tr><th>Subject</th><td>' + this.issueData.subject + '</td></tr>' + 
-                                '<tr><th>Description</th><td>' + this.getPayload() + '</td></tr>' + 
-                                '<tr><th>Last updated</th><td>' + this.issueData.updated_on + '</td></tr>' +                             
-                            '</table>' + 
-                        '</div>' + 
-                    '</div>'
-                );       
-            } else {
-                contentDiv.html(
-                    '<div class="panel panel-default">' + 
-                        '<div class="panel-body">' + 
-                            'None specified' + 
-                        '</div>' + 
-                    '</div>'
-                );
-            }                 
-        }
-    }, this));
+    /* Display issue information (if any) in top right corner of map */  
+    if (this.issueData && !jQuery.isEmptyObject(this.issueData)) {
+        this.target.html(
+            '<table class="table table-condensed" style="margin-bottom:0px">' + 
+                '<tr><th>Issue ID</th><td>' + this.issueData.id + '</td></tr>' + 
+                '<tr><th>Subject</th><td>' + this.issueData.subject + '</td></tr>' + 
+                '<tr><th>Last updated</th><td>' + this.issueData.updated_on + '</td></tr>' +                             
+            '</table>' 
+        );
+    }
     
 };
 
