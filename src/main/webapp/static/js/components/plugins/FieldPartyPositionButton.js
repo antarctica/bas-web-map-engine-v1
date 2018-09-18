16d6052af3b75a35250dc858636a63d3395df81e
Herbert, David J. <darb1@bas.ac.uk>
2018-09-18 18:09:55 +0100

    Field party positions

--- src/main/webapp/static/js/components/plugins/FieldPartyPositionButton.js
+++ src/main/webapp/static/js/components/plugins/FieldPartyPositionButton.js
@@ -34,9 +34,6 @@ magic.classes.FieldPartyPositionButton = function (options) {
     this.featureMap = {};
     
     this.clickedSledge = null;//DELETE
-    
-    /* Flag whether all extra JS and CSS have been loaded */
-    this.extraResourcesLoaded = false;
    
     magic.classes.NavigationBarTool.call(this, options);    
     
@@ -70,40 +67,19 @@ magic.classes.FieldPartyPositionButton = function (options) {
         content: "Loading field party data..."
     })
     .on("shown.bs.popover", jQuery.proxy(function() {
-        if (!this.extraResourcesLoaded) {
-            /* Stylesheets */
-            jQuery("head").append(
-                '<link href="' + magic.config.paths.cdn + '/css/bootstrap-combobox/1.1.6-font-awesome/bootstrap-combobox.min.css" rel="stylesheet" type="text/css">'
-            );
-            jQuery("head").append(
-                '<link href="' + magic.config.paths.cdn + '/css/bootstrap-datetimepicker/4.17.47/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css">'
-            );
-            /* Javascript resources */
-            jQuery("head").append(
-                '<script type="text/javascript" src="' + 
-                    magic.config.paths.cdn + '/js/bootstrap-combobox/1.1.6-font-awesome/bootstrap-combobox.min.js">' + 
-                '</script>'
-            );
-            jQuery("head").append(
-                '<script type="text/javascript" src="' + 
-                    magic.config.paths.cdn + '/js/bootstrap-datetimepicker/4.17.47/bootstrap-datetimepicker.min.js">' + 
-                '</script>'
-            );
-            this.extraResourcesLoaded = true;
-        }
         jQuery.ajax({
             url: magic.config.paths.baseurl + "/fpp/layout",
             dataType: "html",
             success: jQuery.proxy(function(markup) {
-                jQuery(".field-party-popover-content").html(markup);
+                var popoverDiv = jQuery(".field-party-popover-content");
+                popoverDiv.html(markup);            
                 this.activate();
             }, this),
             error: function() {
                 jQuery(".field-party-popover-content").html("Failed to retrieve content for pop-up");
             }
-        });                     
-    }, this));                
-    
+        });     
+    }, this));                    
 };
 
 magic.classes.FieldPartyPositionButton.prototype = Object.create(magic.classes.NavigationBarTool.prototype);
@@ -155,20 +131,10 @@ magic.classes.FieldPartyPositionButton.prototype.onActivate = function() {
             }, this));
             this.layer.getSource().clear();
             this.layer.getSource().addFeatures(feats);
-            /* Update the popover content with fix information */
-            jQuery("." + this.popoverContentClass).html(this.markup());
-            /* Assign handler to activate new sledge */
-            jQuery("#btn-activate-sledge").click(jQuery.proxy(function(evt) {
-                var selection = jQuery("#sel-activate-sledge").val();
-                if (selection != "") {
-                    this.featureMap[selection] = {};
-                    var popoverDiv = jQuery("." + this.popoverContentClass);
-                    popoverDiv.empty();
-                    popoverDiv.html(this.markup());
-                    this.assignHandlers();
-                }
-            }, this));
-            this.assignHandlers();
+            /* Convert the sledge input field to combobox */
+            this.initCombobox("fix-input-sledge", Object.keys(this.featureMap).sort());
+            /* Convert the date input field to a datepicker */
+            this.initDatepicker("fix-input-date");
         }, this),
         error: function() {
             console.log("Failed to get field party positional data - potential network outage?");
@@ -179,6 +145,72 @@ magic.classes.FieldPartyPositionButton.prototype.onActivate = function() {
 magic.classes.FieldPartyPositionButton.prototype.onDeactivate = function() {    
     this.target.popover("hide");
 };
+
+magic.classes.FieldPartyPositionButton.prototype.initCombobox = function(id, opts) {
+    var cbSelect = jQuery("#" + id);
+    if (cbSelect.length > 0) {
+        /* The input exists (we must therefore be admin) */
+        if (!cbSelect.hasClass("combobox")) {
+            /* The input has not been converted */
+            cbSelect.addClass("combobox");
+            cbSelect.combobox({
+                appendId: "-input"
+            });
+            var cbInput = jQuery("#" + id + "-input");
+            cbInput.attr("required", cbSelect.attr("required"));
+            cbInput.attr("data-toggle", "tooltip");
+            cbInput.attr("data-placement", "left");
+            cbInput.attr("title", cbSelect.attr("title"));
+        }
+        cbSelect.empty();
+        for (var j = 0; j < opts.length; j++) {           
+            cbSelect.append(jQuery('<option>', {value: opts[j], text: opts[j]}));
+        }
+    }    
+};
+
+magic.classes.FieldPartyPositionButton.prototype.initDatepicker = function(id) {
+    var dtInput = jQuery("#" + id);
+    if (dtInput.length > 0) {
+        dtInput.addClass("date");
+        dtInput.datetimepicker({
+            viewMode: "days",
+            format: "DD/MM/YYYY"
+        });
+    }
+};
+
+magic.classes.FieldPartyPositionButton.prototype.setComboboxValue = function(id, value) {
+    var cbSelect = jQuery("#" + id);
+    if (cbSelect.length > 0) {
+        /* The input exists, so we must be admin */
+        var cbInput = jQuery("#" + id + "-input");
+        var cbHidden = cbInput.closest("div.combobox-container").find("input[type='hidden']");
+        cbHidden.val(value);
+        /* Activate the corresponding drop-down item (if none, a free-form text string was entered) */
+        var selOpt = jQuery("#" + id).find("option[value='" + value + "']");
+        if (selOpt.length > 0) {
+            /* Activate drop-down element */
+            cbInput.val(selOpt.prop("text"));
+        }
+    }
+};
+
+magic.classes.FieldPartyPositionButton.prototype.setDatepickerValue = function(id, value) {
+    jQuery("#" + id + " :input").val(value);
+};
+
+magic.classes.FieldPartyPositionButton.prototype.getComboboxValue = function(id) {
+    var cbInput = jQuery("#" + id + "-input");
+    var cbHidden = cbInput.closest("div.combobox-container").find("input[type='hidden']");
+    return(cbHidden.val());
+};
+
+magic.classes.FieldPartyPositionButton.prototype.getDatepickerValue = function(id) {
+    var field = jQuery("#" + id + " :input");
+    return(field.val() ? moment(field.val(), "DD/MM/YYYY").format("YYYY-MM-DD") : "");
+};
+
 
 /**
  * Display markup for a positional fix
