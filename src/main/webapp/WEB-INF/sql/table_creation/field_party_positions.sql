146a733f206245937c73486a39d95ad134f03590
Herbert, David J. <darb1@bas.ac.uk>
2018-10-08 13:10:20 +0100

    Table initialisation SQL

--- src/main/webapp/WEB-INF/sql/table_creation/field_party_positions.sql
+++ src/main/webapp/WEB-INF/sql/table_creation/field_party_positions.sql
@@ -3,7 +3,7 @@ CREATE TABLE opsgis2.ops_field_deployments
   id serial,
   sledge varchar(20),
   season varchar(4),
-  fix_date timestamp without time zone,
+  fix_date date,
   updated timestamp without time zone DEFAULT now(),
   people_count integer,
   updater varchar(10),
@@ -45,15 +45,13 @@ CREATE FUNCTION opsgis2.ofd_populate_geometry()
     VOLATILE 
 AS $BODY$
 BEGIN		
-    IF NEW.geometry IS NULL THEN
-        -- Try to populate the_geom from lat,lon fields
-        IF NEW.lat IS NULL AND NEW.lon IS NULL THEN				
-            RAISE EXCEPTION 'Lat and lon may not be null';			
-        ELSE
-            -- Update geometry to match lat,lon
-            NEW.geometry := st_transform(st_setsrid(st_makepoint(NEW.lon, NEW.lat),4326), 3031);				
-        END IF;		
-    END IF;
+    -- Try to populate the_geom from lat,lon fields
+    IF NEW.lat IS NULL AND NEW.lon IS NULL THEN				
+        RAISE EXCEPTION 'Lat and lon may not be null';			
+    ELSE
+        -- Update geometry to match lat,lon
+        NEW.geometry := st_transform(st_setsrid(st_makepoint(NEW.lon, NEW.lat),4326), 3031);				
+    END IF;		
     RETURN NEW;
 END
 $BODY$;
@@ -61,9 +59,69 @@ $BODY$;
 ALTER FUNCTION opsgis2.ofd_populate_geometry()
     OWNER TO add;
 
-
 CREATE TRIGGER populate_geometry_from_lat_lon
     BEFORE INSERT OR UPDATE 
     ON opsgis2.ops_field_deployments
     FOR EACH ROW
     EXECUTE PROCEDURE opsgis2.ofd_populate_geometry();
+
+CREATE TABLE opsgis2.ops_field_deployments_history
+(
+  id serial,
+  fp_id integer,
+  sledge varchar(20),
+  season varchar(4),
+  fix_date date,
+  updated timestamp without time zone DEFAULT now(),
+  people_count integer,
+  updater varchar(10),
+  lat numeric,
+  lon numeric,
+  height numeric,
+  notes text,
+  geometry geometry(POINT, 3031),
+  CONSTRAINT fpp_history_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE opsgis2.ops_field_deployments_history
+  OWNER TO add;
+
+CREATE FUNCTION opsgis2.ops_field_deployments_write_undo()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF 
+    COST 100
+AS $BODY$
+BEGIN
+    IF (TG_OP = 'DELETE') THEN
+        INSERT INTO opsgis2.ops_field_deployments_history 
+        (fp_id, sledge, season, fix_date, updated, people_count, updater, lat, lon, height, notes)
+        VALUES (SELECT id, sledge, season, fix_date, CURRENT_TIMESTAMP, people_count, updater, lat, lon, height, notes
+        FROM opsgis2.ops_field_deployments WHERE id = OLD.id);
+        RETURN OLD;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        INSERT INTO opsgis2.ops_field_deployments_history 
+        (fp_id, sledge, season, fix_date, updated, people_count, updater, lat, lon, height, notes)
+        VALUES (SELECT id, sledge, season, fix_date, CURRENT_TIMESTAMP, people_count, updater, lat, lon, height, notes 
+        FROM opsgis2.ops_field_deployments WHERE id = NEW.id);
+        RETURN NEW;
+    END IF;
+    RETURN NULL;
+END
+$BODY$;
+
+ALTER FUNCTION opsgis2.ops_field_deployments_write_undo()
+    OWNER TO add;
+
+COMMENT ON FUNCTION opsgis2.ops_field_deployments_write_undo()
+    IS 'Saves the a fix record pre-edit/delete';
+
+CREATE TRIGGER write_to_history
+    BEFORE DELETE OR UPDATE 
+    ON opsgis2.ops_field_deployments
+    FOR EACH ROW
+    EXECUTE PROCEDURE opsgis2.ops_field_deployments_write_undo();
