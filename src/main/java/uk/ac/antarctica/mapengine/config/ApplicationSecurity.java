558f324d598b6b9ef3227257bd2b9ef5bf5c021c
Herbert, David J. <darb1@bas.ac.uk>
2018-12-18 15:28:13 +0000

    Back off config changes (too many redirects errors)

--- src/main/java/uk/ac/antarctica/mapengine/config/ApplicationSecurity.java
+++ src/main/java/uk/ac/antarctica/mapengine/config/ApplicationSecurity.java
@@ -4,8 +4,12 @@
 package uk.ac.antarctica.mapengine.config;
 
 import uk.ac.antarctica.mapengine.components.GeoserverAuthenticationProvider;
+import java.io.IOException;
 import java.util.regex.Pattern;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +19,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import uk.ac.antarctica.mapengine.config.ApplicationSecurity.CsrfSecurityRequestMatcher;
@@ -27,54 +34,41 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
-
+    
     @Bean
     public GeoserverAuthenticationProvider geoserverAuthenticationProvider() {
-        return (new GeoserverAuthenticationProvider());
+        return(new GeoserverAuthenticationProvider());
     }
 
-    public class RefererRedirectionAuthenticationSuccessHandler
-            extends SimpleUrlAuthenticationSuccessHandler
-            implements AuthenticationSuccessHandler {
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        CustomLoginSuccessHandler handler = new CustomLoginSuccessHandler();
+        handler.setUseReferer(true);
+        return (handler);
+    }
 
-        public RefererRedirectionAuthenticationSuccessHandler() {
-            super();
-            setUseReferer(true);
+    /* See http://thinkinginsoftware.blogspot.co.uk/2011/07/redirect-after-login-to-requested-page.html */
+    public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+        public CustomLoginSuccessHandler() {
+            //setDefaultTargetUrl(defaultTargetUrl);
         }
 
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+            HttpSession session = request.getSession();
+            if (session != null) {
+                SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+                String redirectTo = savedRequest != null ? savedRequest.getRedirectUrl() : "/home";                                        
+                System.out.println("savedRequest will redirect to " + redirectTo);
+                getRedirectStrategy().sendRedirect(request, response, redirectTo);                
+            } else {
+                super.onAuthenticationSuccess(request, response, authentication);
+            }
+        }
     }
 
-//    @Bean
-//    public AuthenticationSuccessHandler successHandler() {
-//        CustomLoginSuccessHandler handler = new CustomLoginSuccessHandler();
-//        handler.setUseReferer(true);
-//        return (handler);
-//    }
-//
-//    /* See http://thinkinginsoftware.blogspot.co.uk/2011/07/redirect-after-login-to-requested-page.html */
-//    public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
-//
-//        public CustomLoginSuccessHandler() {
-//            //setDefaultTargetUrl(defaultTargetUrl);
-//        }
-//
-//        @Override
-//        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-//            HttpSession session = request.getSession();
-//            if (session != null) {
-//                SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
-//                String redirectTo = savedRequest != null ? savedRequest.getRedirectUrl() : "/home";                                        
-//                System.out.println("savedRequest will redirect to " + redirectTo);
-//                getRedirectStrategy().sendRedirect(request, response, redirectTo);                
-//            } else {
-//                super.onAuthenticationSuccess(request, response, authentication);
-//            }
-//        }
-//    }
-
-    /*
-     * See https://blogs.sourceallies.com/2014/04/customizing-csrf-protection-in-spring-security/ - want to protect GET requests to /ogc/proxy for security reasons
-     */
+    /* See https://blogs.sourceallies.com/2014/04/customizing-csrf-protection-in-spring-security/ - want to protect GET requests to /ogc/proxy for security reasons */
     public class CsrfSecurityRequestMatcher implements RequestMatcher {
 
         private final Pattern allowedMethods = Pattern.compile("^(HEAD|TRACE|OPTIONS)$");
@@ -94,62 +88,54 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        /*
-         * Form-based authentication of some kind
-         */
+                   
+        /* Form-based authentication of some kind */
         System.out.println("Ordinary authentication via login form");
-        http
-                .authorizeRequests()
-                .antMatchers("/*.ico", "/static/**", "/ping", "/home/**", "/homed/**",
-                        "/maps/dropdown/**", "/maps/name/**", "/maps/id/**", "/feedback",
-                        "/embedded_maps/dropdown/**", "/embedded_maps/name/**", "/embedded_maps/id/**",
-                        "/usermaps/data", "/ogc/**", "/endpoints/get/**", "/endpoints/dropdown",
-                        "/thumbnail/show/**", "/prefs/get", "/gs/**").permitAll()
-                .antMatchers("/creator", "/creatord", "/embedded_creator", "/embedded_creatord",
-                        "/restricted/**", "/restrictedd/**",
-                        "/userlayers/**", "/prefs/set", "/feedback/issues/**",
-                        "/maps/save", "/maps/update/**", "/maps/delete/**", "/maps/deletebyname/**",
-                        "/embedded_maps/save", "/embedded_maps/update/**", "/embedded_maps/delete/**", "/embedded_maps/deletebyname/**",
-                        "/usermaps/save", "/usermaps/update/**", "/usermaps/delete/**",
-                        "/thumbnail/save/**", "/thumbnail/delete/**", "/rothera_reports/**", "/fpp/**",
-                        "/endpoint_manager", "/endpoints/save", "/endpoints/update/**", "/endpoints/delete/**")
-                .fullyAuthenticated()
-                .and()
-                .formLogin()
-                .loginPage(env.getProperty("default.login_page", "/login"))
-                .successHandler(new RefererRedirectionAuthenticationSuccessHandler())
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl(env.getProperty("default.logout_page", "/home"))
-                .permitAll();
-
-        /*
-         * Check for require_ssl environment property
-         */
+        http                   
+            .authorizeRequests()
+            .antMatchers("/*.ico", "/static/**", "/ping", "/home/**", "/homed/**",
+                    "/maps/dropdown/**", "/maps/name/**", "/maps/id/**", "/feedback",
+                    "/embedded_maps/dropdown/**", "/embedded_maps/name/**", "/embedded_maps/id/**",
+                    "/usermaps/data", "/ogc/**", "/endpoints/get/**", "/endpoints/dropdown",
+                    "/thumbnail/show/**", "/prefs/get", "/gs/**").permitAll()
+            .antMatchers("/creator", "/creatord", "/embedded_creator", "/embedded_creatord",
+                    "/restricted/**", "/restrictedd/**",
+                    "/userlayers/**", "/prefs/set", "/feedback/issues/**",
+                    "/maps/save", "/maps/update/**", "/maps/delete/**", "/maps/deletebyname/**",
+                    "/embedded_maps/save", "/embedded_maps/update/**", "/embedded_maps/delete/**", "/embedded_maps/deletebyname/**",
+                    "/usermaps/save", "/usermaps/update/**", "/usermaps/delete/**",
+                    "/thumbnail/save/**", "/thumbnail/delete/**", "/rothera_reports/**", "/fpp/**",
+                    "/endpoint_manager", "/endpoints/save", "/endpoints/update/**", "/endpoints/delete/**")
+            .fullyAuthenticated()
+            .and()
+            .formLogin()
+            .loginPage(env.getProperty("default.login_page", "/login"))
+            .successHandler(successHandler())
+            .permitAll()
+            .and()
+            .logout()
+            .logoutSuccessUrl(env.getProperty("default.logout_page", "/home"))
+            .permitAll();
+        
+        /* Check for require_ssl environment property */
         String requireSsl = env.getProperty("security.require-ssl", "false");
         if (requireSsl.equals("true")) {
             http.requiresChannel().anyRequest().requiresSecure();
         }
-
-        /*
-         * Apply CSRF checks to all POST|PUT|DELETE requests, and GET to selected ones
-         */
-        http.csrf().requireCsrfProtectionMatcher(new CsrfSecurityRequestMatcher());
+            
+        /* Apply CSRF checks to all POST|PUT|DELETE requests, and GET to selected ones */
+        http.csrf().requireCsrfProtectionMatcher(new CsrfSecurityRequestMatcher());        
     }
-
+    
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/static/**");
     }
 
     @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        /*
-         * Authentication against the local Geoserver instance (incorporates LDAP)
-         */
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {                
+        
+        /* Authentication against the local Geoserver instance (incorporates LDAP) */
         if (env.getProperty("authentication.geoserver").equals("yes")) {
             auth.authenticationProvider(geoserverAuthenticationProvider());
         }
