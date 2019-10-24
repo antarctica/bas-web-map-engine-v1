362df41c97096e191f0f5070a5d3e53aa1bbc348
Felix Fennell <felnne@bas.ac.uk>
2019-10-24 16:58:09 +0100

    Adding sample data for field party deployments

---
+++ src/main/webapp/WEB-INF/sql/table_population/docker/test-data-plugin-field-parties.sql
@@ -0,0 +1,71 @@
+-- Field party positions plugin --
+INSERT INTO webmap.field_deployments (
+      sledge,
+      season,
+      fix_date,
+      updated,
+      people_count,
+      updater,
+      lat,
+      lon,
+      height,
+      notes
+) VALUES (
+      'Alpha',
+      '1819',
+      '2018-12-10',
+      '2018-12-12 11:22:15.276184',
+      2,
+      'XX',
+      -74.86155,
+      -71.5883333333333,
+      4500,
+      'Testing entry for the 1819 field season'
+);
+
+INSERT INTO webmap.field_deployments (
+      sledge,
+      season,
+      fix_date,
+      updated,
+      people_count,
+      updater,
+      lat,
+      lon,
+      height,
+      notes
+) VALUES (
+      'Bravo',
+      '1920',
+      '2019-10-14',
+      '2019-10-14 22:22:22',
+      2,
+      'XX',
+      -71,
+      -75,
+      3608,
+      'Testing entry for the 1920 field season'
+);
+INSERT INTO webmap.field_deployments (
+      sledge,
+      season,
+      fix_date,
+      updated,
+      people_count,
+      updater,
+      lat,
+      lon,
+      height,
+      notes
+) VALUES (
+      'Bravo',
+      '1920',
+      '2019-10-28',
+      '2019-10-28 22:22:22',
+      2,
+      'XX',
+      -72,
+      -72,
+      3608,
+      'Testing entry for the 1920 field season'
+);
